66277936458df5e7ec3b71b295334bd0be0df9a6
ngovietbao <ngothivietbao@gmail.com>
2018-05-22 03:58:00 +0700

    delete server

--- client/src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDTvYfKmyRD7SsdMHMAPs9skW2-192QRL8",
-  authDomain: "assignment-twitsplit.firebaseapp.com",
-  databaseURL: "https://assignment-twitsplit.firebaseio.com",
-  projectId: "assignment-twitsplit",
-  storageBucket: "assignment-twitsplit.appspot.com",
-  messagingSenderId: "260069019985"
-};
